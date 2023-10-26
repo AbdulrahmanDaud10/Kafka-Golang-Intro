0a47a16bc8a17c6185819a587de8adfdfb3222ec
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2023-10-26 17:01:17 +0300

    Consumer Implementation

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -1,1 +1,28 @@
 package consumer
+
+import (
+	kafkaConfig "github.com/AbdulrahmanDaud10/golang-kafka-intro/config"
+
+	"log"
+
+	"github.com/IBM/sarama" //Sarama is an MIT-licensed Go client library for Apache Kafka
+)
+
+func Consume(topic string) {
+	config := sarama.NewConfig()
+
+	consumer, err := sarama.NewConsumer([]string{kafkaConfig.CONST_HOST}, config)
+	if err != nil {
+		log.Fatal("NewConsumer err: ", err)
+	}
+	defer consumer.Close()
+
+	partitionConsumer, err := consumer.ConsumePartition(topic, 0, sarama.OffsetNewest)
+	if err != nil {
+		log.Fatal("ConsumePartition err: ", err)
+	}
+	defer partitionConsumer.Close()
+	for message := range partitionConsumer.Messages() {
+		log.Printf("[Consumer] partitionid: %d; offset:%d, value: %s\n", message.Partition, message.Offset, string(message.Value))
+	}
+}
